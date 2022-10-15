7a55a404eaef7eb31d4253ab63f7862127eb9ef7
Kyung-Wan Woo <wkw8402@berkeley.edu>
2022-10-14 23:55:25 -0700

    lecture organized

---
+++ CounterApp/CounterApp/CounterAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CounterAppApp.swift
+//  CounterApp
+//
+//  Created by Paul Woo on 14/10/22.
+//
+
+import SwiftUI
+
+@main
+struct CounterAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
